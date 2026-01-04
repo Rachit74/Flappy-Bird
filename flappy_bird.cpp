462bbbbfbe96d3c2147cfa857707e47a188cdd12
rachit74 <rachithooda09@gmail.com>
2026-01-04 11:16:36 +0530

    gravity logic on bird

--- flappy_bird.cpp
+++ flappy_bird.cpp
@@ -4,12 +4,44 @@ int main() {
 
     sf::RenderWindow window(sf::VideoMode(800,600), "Flappy bird c++");
 
+    // Clock to measure time between frames (deltaTime)
+    sf::Clock clock;
+
+    sf::CircleShape Bird(25.f);
+    Bird.setFillColor(sf::Color::Green);
+    Bird.setPosition(200.f, 300.f);
+
+    // How fast the bird is moving up/down
+    float velocity = 0.f;
+
+    const float g = 700.f;
+
+    // game loop
     while (window.isOpen()) {
+        
+        // event loop
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
+
+        // Time passed since last frame (in seconds)
+        float dt = clock.restart().asSeconds();
+
+        // Gravity makes the bird fall faster every frame
+        velocity += g * dt;
+
+        // Get the bird's current position
+        // Move the bird down using its speed
+        // Apply the new position to the bird
+        sf::Vector2f birdPosition = Bird.getPosition();
+        birdPosition.y += velocity * dt;
+        Bird.setPosition(birdPosition);
+
+        window.clear();
+        window.draw(Bird);
+        window.display();
     }
 }
