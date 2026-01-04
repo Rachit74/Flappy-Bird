b2d3df622940f3544d4a27549a41f106beee43d7
rachit74 <rachithooda09@gmail.com>
2026-01-04 10:55:08 +0530

    Basic Setup

---
+++ flappy_bird.cpp
@@ -0,0 +1,15 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+
+    sf::RenderWindow window(sf::VideoMode(800,600), "Flappy bird c++");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+}
