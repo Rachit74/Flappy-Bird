b05f95b8190b78ababd996b1697b31064e7eae85
rachit74 <rachithooda09@gmail.com>
2026-01-05 22:06:15 +0530

    use struct for Pipe Pairs

--- flappy_bird.cpp
+++ flappy_bird.cpp
@@ -1,8 +1,50 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 
 #include "constants.h"
 #include "gap_gen.h"
+
+struct PipePair {
+    sf::RectangleShape top;
+    sf::RectangleShape bottom;
+
+
+    PipePair(float start_x_position) {
+        top.setSize({PIPE_WIDTH, PIPE_HEIGHT});
+        bottom.setSize({PIPE_WIDTH, PIPE_HEIGHT});
+        
+        top.setFillColor(sf::Color::Red);
+        bottom.setFillColor(sf::Color::Red);
+
+        int gapY = gapGen();
+        top.setPosition(start_x_position, gapY - PIPE_HEIGHT);
+        bottom.setPosition(start_x_position, gapY + PIPE_GAP);
+
+    }
+
+    void update(float dt) {
+        float x = top.getPosition().x - PIPE_SPEED * dt;
+
+        top.setPosition(x, top.getPosition().y);
+        bottom.setPosition(x, bottom.getPosition().y);
+    }
+
+    void reset() {
+        float x = top.getPosition().x;
+
+        if (x + PIPE_WIDTH < 0) {
+            int gapY = gapGen();
+
+            top.setPosition(WINDOW_WIDTH, gapY - PIPE_HEIGHT);
+            bottom.setPosition(WINDOW_WIDTH, gapY + PIPE_GAP);
+        }
+    }
+
+    bool collidesWith(const sf::CircleShape& bird) const {
+        return bird.getGlobalBounds().intersects(top.getGlobalBounds()) || bird.getGlobalBounds().intersects(bottom.getGlobalBounds());
+    }
+};
 
 
 // check window border hit
@@ -24,66 +66,13 @@ void checkBorderHit(sf::CircleShape& Bird) {
     }
 }
 
-// move pipe function
-void movePipe(sf::RectangleShape& topPipe, sf::RectangleShape& bottomPipe, float dt) {
-
-    // we can use Vector2f too but that would be redundant
-    /*
-    sf::Vector2f pos_1 = topPipe.getPosition();
-    sf::Vector2f pos_2 = bottomPipe.getPosition();
-
-    pos_1.x -= PIPE_SPEED * dt;
-    pos_2.x -= PIPE_SPEED * dt;
-
-    topPipe.setPosition(pos_1.x, pos_1.y);
-    bottomPipe.setPosition(pos_2.x, pos_2.y);
-    */
-
-    float x = topPipe.getPosition().x - PIPE_SPEED * dt;
-
-    topPipe.setPosition(x, topPipe.getPosition().y);
-    bottomPipe.setPosition(x, bottomPipe.getPosition().y);
-
-
-}
-
-
-/*
-restPipes function
-resets pipe postion to far right of the window so they can reapper
-uses the gapGen function to generate the Y position of the gap box
-adjusts the height of the pipes according to the randomly generated value by gapGen
-*/
-void resetPipes(sf::RectangleShape& topPipe, sf::RectangleShape& bottomPipe) {
-
-    float x = topPipe.getPosition().x;
-
-    if (x + PIPE_WIDTH < 0) {
-        
-        int gapY = gapGen();
-
-        topPipe.setPosition(WINDOW_WIDTH, gapY - PIPE_HEIGHT);
-        bottomPipe.setPosition(WINDOW_WIDTH, gapY + PIPE_GAP);
-    }
-}
-
-/* initPipes function
-initilizes the first two pipe pairs to have gap between them
-solves the overlap bug because of increased PIPE_HEIGHT
-similar work as the restPipes function but just called once when the games starts for first two pairs
-*/
-void initPipes(sf::RectangleShape& topPipe, sf::RectangleShape& bottomPipe, float startX) {
-    int gapY = gapGen();
-
-    topPipe.setPosition(startX, gapY - PIPE_HEIGHT);
-    bottomPipe.setPosition(startX, gapY + PIPE_GAP);
-}
-
-
-
 
 int main() {
 
+    // Pipes Vector;
+    std::vector<PipePair> pipes;
+    pipes.emplace_back(WINDOW_WIDTH);
+    pipes.emplace_back(WINDOW_WIDTH + 400);
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH,WINDOW_HEIGHT), "Flappy bird c++");
 
@@ -95,32 +84,6 @@ int main() {
     sf::CircleShape Bird(25.f);
     Bird.setFillColor(sf::Color::Green);
     Bird.setPosition(100.f, 300.f);
-
-
-    // PIPE PAIR 1
-    // Top pipe
-    sf::RectangleShape topPipe(sf::Vector2f(PIPE_WIDTH, PIPE_HEIGHT));
-    topPipe.setFillColor(sf::Color::Red);
-    topPipe.setPosition(800.f,0);
-
-    // Bottom pipe
-    sf::RectangleShape bottomPipe(sf::Vector2f(PIPE_WIDTH, PIPE_HEIGHT));
-    bottomPipe.setFillColor(sf::Color::Red);
-    bottomPipe.setPosition(800,600-PIPE_HEIGHT);
-
-    // PIPE PAIR 2 (Pipe pair 2 will spawn to further right on the screen)
-    // Top Pipe
-    sf::RectangleShape topPipe2(sf::Vector2f(PIPE_WIDTH, PIPE_HEIGHT));
-    topPipe2.setFillColor(sf::Color::Red);
-    topPipe2.setPosition(WINDOW_WIDTH + 400, 0);
-
-    // Bottom Pipe
-    sf::RectangleShape bottomPipe2(sf::Vector2f(PIPE_WIDTH, PIPE_HEIGHT));
-    bottomPipe2.setFillColor(sf::Color::Red);
-    bottomPipe2.setPosition(WINDOW_WIDTH + 400, WINDOW_HEIGHT - PIPE_HEIGHT);
-
-    initPipes(topPipe, bottomPipe, WINDOW_WIDTH);
-    initPipes(topPipe2, bottomPipe2, WINDOW_WIDTH + 400);
 
 
 
@@ -159,20 +122,28 @@ int main() {
         birdPosition.y += velocity * dt;
         Bird.setPosition(birdPosition);
 
-        checkBorderHit(Bird);
 
-        movePipe(topPipe, bottomPipe, dt);
-        movePipe(topPipe2, bottomPipe2, dt);
 
-        resetPipes(topPipe, bottomPipe);
-        resetPipes(topPipe2, bottomPipe2);
+
+        for (auto& pipe: pipes) {
+            pipe.update(dt);
+            pipe.reset();
+            if (pipe.collidesWith(Bird)) {
+                std::cout << "Hit\n";
+            } else {
+                std::cout << "Not Hitting\n";
+            }
+            
+        }
 
         window.clear();
+
+        for (const auto& pipe: pipes) {
+            window.draw(pipe.top);
+            window.draw(pipe.bottom);
+        }
+
         window.draw(Bird);
-        window.draw(topPipe);
-        window.draw(bottomPipe);
-        window.draw(topPipe2);
-        window.draw(bottomPipe2);
         window.display();
     }
 }
